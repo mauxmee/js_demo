86c56208bd06afc30b21c841666269d19b06f206
Kai <mauxmee@gmail.com>
2019-03-14 14:37:53 +1100

    update index.html for trackJs.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,10 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
 //import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 export default {
   //debug: true,
@@ -44,16 +48,40 @@ export default {
   //   }
   // },
   optimization: {
+    // splitChunks: {
+    //   chunks: 'all',
+    //   minSize: 0
+    // },
     splitChunks: {
-      chunks: 'all',
-      minSize: 0
-    }
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
 
   plugins: [
 
-    // // Generate an external css file with a hash in the filename
-    // new ExtractTextPlugin('[name].[contenthash].css'),
+    // Generate an external css file with a hash in the filename
+    //new ExtractTextPlugin('[name].[contenthash].css'),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
 
     // Hash the files using MD5 so that their names change when the content changes.
     new WebpackMd5Hash(),
@@ -61,16 +89,18 @@ export default {
     // Elinimate duplicated packages when generating bundle
     //new webpack.optimize.DedupePlugin(), <--- removed from webpack already
     // minify JS
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        sourceMap: true,
-        warnings: false,
-        ie8: false,
-        output: {
-          comments: false
-        }
-      }
-    }),
+    // new UglifyJsPlugin({
+    //   sourceMap: true, // need to enable sourceMap here
+    //   cache: true,
+    //   parallel: true,
+    //   uglifyOptions: {
+    //     warnings: false,
+    //     ie8: false,
+    //     output: {
+    //       comments: false
+    //     }
+    //   }
+    // }),
 
     // // Use CommonsChunkPlugin to create a separate bundle
     // // of vendor libraries so that they're cached separately.
@@ -99,8 +129,17 @@ export default {
   module: {
     rules: [
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
-       { test: /\.css$/, use: 'css-loader' }
-//{test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+      //{ test: /\.css$/, use: 'css-loader' }
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }
+      //{ test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
     ]
   },
   resolve: {
